777f98b38565d256c4b32f96f1fefdcfa3dfd054
Juhee Noh <nohjuhee227@gmail.com>
2023-05-30 18:13:51 +0900

    try. concat() 활용하여 expenses 상태 업데이트

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "치약",
+    amount: 1000,
+    date: new Date(2023, 2, 14),
+  },
+  {
+    id: "e2",
+    title: "크랜베리아몬드그래놀라350g",
+    amount: 6180,
+    date: new Date(2023, 2, 8),
+  },
+  {
+    id: "e3",
+    title: "우유식빵",
+    amount: 2600,
+    date: new Date(2023, 2, 8),
+  },
+  {
+    id: "e4",
+    title: "남양고소한락토",
+    amount: 2980,
+    date: new Date(2023, 2, 8),
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "치약",
-      amount: 1000,
-      date: new Date(2023, 2, 14),
-    },
-    {
-      id: "e2",
-      title: "크랜베리아몬드그래놀라350g",
-      amount: 6180,
-      date: new Date(2023, 2, 8),
-    },
-    {
-      id: "e3",
-      title: "우유식빵",
-      amount: 2600,
-      date: new Date(2023, 2, 8),
-    },
-    {
-      id: "e4",
-      title: "남양고소한락토",
-      amount: 2980,
-      date: new Date(2023, 2, 8),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
     console.log(expense);
+    setExpenses(expenses.concat(expense));
   };
 
   // return React.createElement(
